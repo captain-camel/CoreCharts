58ec1a68fd4d1ce33b6d151e2f6684cb07a8e138
captain-camel <cameron.delong@icloud.com>
2021-10-07 11:58:36 -0600

    Added `rounded` method to `CGSize`

---
+++ Sources/CoreCharts/Extensions/CGSize.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cameron Delong on 10/7/21.
+//
+
+import CoreGraphics
+
+extension CGSize {
+    /// Returns the `CGSize` with the width and height rounded.
+    func rounded(places: Int = 0) -> Self {
+        return CGSize(width: round(width * pow(10, CGFloat(places))) / pow(10, CGFloat(places)), height: round(height * pow(10, CGFloat(places))) / pow(10, CGFloat(places)))
+    }
+}
