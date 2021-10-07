70455bb4207c98ae0f270dc612720408f62b9ac8
captain-camel <cameron.delong@icloud.com>
2021-10-07 11:54:10 -0600

    Added `readSize` method to `View`

---
+++ Sources/CoreCharts/Helpers/ReadSize.swift
@@ -0,0 +1,40 @@
+//
+//  ReadSize.swift
+//  
+//
+//  Created by Cameron Delong on 10/7/21.
+//
+
+import SwiftUI
+
+extension View {
+    /// Updates a `Binding` when the size of a view changes.
+    @ViewBuilder func readSize(size: Binding<CGSize>) -> some View {
+        self
+            .background(SizeReader(size: size))
+    }
+}
+
+/// A view with a binding to its own size.
+struct SizeReader: View {
+    // MARK: Properties
+    /// The size of the view.
+    @Binding var size: CGSize
+    
+    // MARK: Body
+    var body: some View {
+        GeometryReader { geometry in
+            Color.clear
+                .onAppear {
+                    DispatchQueue.main.async {
+                        size = geometry.size.rounded()
+                    }
+                }
+                .onChange(of: geometry.size) { _ in
+                    DispatchQueue.main.async {
+                        size = geometry.size.rounded()
+                    }
+                }
+        }
+    }
+}
