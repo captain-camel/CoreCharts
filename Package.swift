234b47b642101724ef7f7a8bfd29a3de9b88af5c
captain-camel <cameron.delong@icloud.com>
2021-10-06 21:04:39 -0600

    Listed supported platforms in `Package.swift`

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CoreCharts",
+    platforms: [
+            .iOS(.v13), .watchOS(.v6), .macOS(.v11)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
