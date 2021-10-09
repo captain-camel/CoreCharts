b55065a145208f4c3a010bbbdf14fb8ffb9f215a
captain-camel <cameron.delong@icloud.com>
2021-10-09 14:40:52 -0600

    Renamed `LineChartYAxisLabels` to `ChartYAxisLabels`

--- Sources/CoreCharts/ChartYAxisLabels.swift
+++ Sources/CoreCharts/ChartYAxisLabels.swift
@@ -7,17 +7,17 @@
 
 import SwiftUI
 
-/// The labels on the Y axis of a `LineChart`.
-struct LineChartYAxisLabels: View {
+/// The labels on the Y axis of a chart.
+struct ChartYAxisLabels: View {
     // MARK: Properties
     /// The data displayed.
     let data: [Double]
     
-    /// The height of the view.
-    let height: CGFloat
-    
     /// The number of horizontal lines the chart shows.
     var horizontalLines: Int
+    
+    /// The size of the view.
+    @State var size = CGSize.zero
     
     /// The labels on the y axis.
     var yAxisLabels: [Double] {
@@ -38,7 +38,7 @@ struct LineChartYAxisLabels: View {
         var points: [CGFloat] = []
         
         for row in 0..<horizontalLines {
-            points.append((height / CGFloat(horizontalLines - 1)) * CGFloat(row))
+            points.append((size.height / CGFloat(horizontalLines - 1)) * CGFloat(row))
         }
         
         return points
@@ -46,11 +46,15 @@ struct LineChartYAxisLabels: View {
     
     // MARK: Body
     var body: some View {
-        ForEach(0..<horizontalLines, id: \.self) { row in
-            Text(String(format: "%.2f", yAxisLabels[row]))
-                .offset(y: yAxisPoints[row] - height / 2)
-                .foregroundColor(Color(.systemGray4))
-                .font(.caption)
+        ZStack(alignment: .trailing) {
+            ForEach(0..<horizontalLines, id: \.self) { row in
+                Text(String(format: "%.2f", yAxisLabels[row]))
+                    .offset(y: yAxisPoints[row] - size.height / 2)
+                    .foregroundColor(Color(.systemGray4))
+                    .font(.caption)
+            }
         }
+        .frame(maxHeight: .infinity)
+        .readSize($size)
     }
 }
