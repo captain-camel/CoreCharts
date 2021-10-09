b1585f063fa1ca2b56468f8c3fc604b2a68e612a
captain-camel <cameron.delong@icloud.com>
2021-10-09 14:37:24 -0600

    Added content to `BarChart` view

--- Sources/CoreCharts/Bar Chart/BarChart.swift
+++ Sources/CoreCharts/Bar Chart/BarChart.swift
@@ -11,7 +11,13 @@ import SwiftUI
 public struct BarChart: View {
     // MARK: Properties
     /// The data displayed.
-    let data: [(label: String?, value: Double, id: UUID)]
+    private let data: [(label: String?, value: Double, id: UUID)]
+    
+    /// The style defining the colors of the chart.
+    private var style: BarChartStyle = .blue
+    
+    /// The number of horizontal lines the chart shows.
+    private var horizontalLines = 5
     
     /// The size of the view.
     @State private var size = CGSize.zero
@@ -29,20 +35,40 @@ public struct BarChart: View {
     
     // MARK: Body
     public var body: some View {
-        ZStack(alignment: .bottom) {
-            LineChartBackground(horizontalLines: 5)
-                .frame(height: 240)
+        HStack {
+            ChartYAxisLabels(data: data.map(\.value), horizontalLines: horizontalLines)
             
-            BarChartBars(data: data.map { ($0.value, UUID()) })
-        }
-        .frame(height: 240)
+            ZStack(alignment: .bottom) {
+                ChartBackground(horizontalLines: horizontalLines)
+                    .frame(height: 240)
+                
+                BarChartBars(data: data.map { ($0.value, UUID()) }, style: style)
+            }
+            .frame(height: 240)
         .readSize($size)
+        }
+    }
+    
+    // MARK: Methods
+    /// Sets the style of the chart.
+    public func style(_ style: BarChartStyle) -> Self {
+        var newView = self
+        newView.style = style
+        return newView
+    }
+    
+    /// Sets the number of horizontal lines in the background of the chart.
+    public func horizontalLines(_ lines: Int) -> Self {
+        var newView = self
+        newView.horizontalLines = lines
+        return newView
     }
 }
 
 struct BarChart_Previews: PreviewProvider {
     static var previews: some View {
-        BarChart(data: [-20, -10, -41, -65, -76, -50, -44, -31, -66, -40, -40, -70, -70, -54, -30, -30, -60, -67, -20, -53, 87, -63, -70, -25, -65, -11, -40, -60, -14, -64])
+        BarChart(data: [20, 10, 41, -1, -76, -50, 44, 31, -66, 40, 40, 70, -70, -54, -30, -30, -60, -67, -20, 53, 87, 63, 70, -25, -65, -11, -40, -60, -14, -64])
+            .style(.teal)
             .padding()
     }
 }
