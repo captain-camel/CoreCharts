bfdfd1d62dd545d832ec2065751510d63e01371d
captain-camel <cameron.delong@icloud.com>
2021-10-09 14:42:19 -0600

    Changed static properties of `LineChartStyle` to type `Self` instead of `LineChartStyle`

--- Sources/CoreCharts/Line Chart/LineChartStyle.swift
+++ Sources/CoreCharts/Line Chart/LineChartStyle.swift
@@ -11,28 +11,28 @@ import SwiftUI
 public struct LineChartStyle {
     // MARK: Properties
     /// A blue/purple gradient.
-    public static let blue = LineChartStyle(
+    public static let blue = Self(
         startColor: Color(red: 120/255, green: 120/255, blue: 255/255),
         endColor: Color(red: 110/255, green: 235/255, blue: 255/255),
         gradientColor: Color(red: 170/255, green: 255/255, blue: 255/255)
     )
     
     /// An orange/red gradient.
-    public static let orange = LineChartStyle(
-        startColor: Color(red: 235/255, green: 35/255, blue: 0/255),
+    public static let orange = Self(
+        startColor: Color(red: 237/255, green: 74/255, blue: 9/255),
         endColor: Color(red: 255/255, green: 150/255, blue: 95/255),
         gradientColor: Color(red: 255/255, green: 179/255, blue: 135/255)
     )
     
     /// A green gradient.
-    public static let green = LineChartStyle(
-        startColor: Color(red: 5/255, green: 70/122, blue: 5/255),
+    public static let green = Self(
+        startColor: Color(red: 50/255, green: 130/255, blue: 50/255),
         endColor: Color(red: 120/255, green: 230/255, blue: 150/255),
         gradientColor: Color(red: 181/255, green: 255/255, blue: 182/255)
     )
     
     /// A blue/purple gradient with a glow.
-    public static let blueGlow = LineChartStyle(
+    public static let blueGlow = Self(
         startColor: Color(red: 120/255, green: 120/255, blue: 255/255),
         endColor: Color(red: 110/255, green: 235/255, blue: 255/255),
         gradientColor: Color(red: 170/255, green: 255/255, blue: 255/255),
@@ -40,16 +40,16 @@ public struct LineChartStyle {
     )
     
     /// An orange/red gradient with a glow.
-    public static let orangeGlow = LineChartStyle(
-        startColor: Color(red: 235/255, green: 35/255, blue: 0/255),
+    public static let orangeGlow = Self(
+        startColor: Color(red: 237/255, green: 74/255, blue: 9/255),
         endColor: Color(red: 255/255, green: 150/255, blue: 95/255),
         gradientColor: Color(red: 255/255, green: 158/255, blue: 120/255),
         glowColor: .orange
     )
     
     /// A green gradient with a glow.
-    public static let greenGlow = LineChartStyle(
-        startColor: Color(red: 5/255, green: 70/122, blue: 5/255),
+    public static let greenGlow = Self(
+        startColor: Color(red: 50/255, green: 130/255, blue: 50/255),
         endColor: Color(red: 120/255, green: 230/255, blue: 150/255),
         gradientColor: Color(red: 181/255, green: 255/255, blue: 182/255),
         glowColor: .green
