4e946f5d7eb9b68f92e04f7100bd6fd06ddaaf73
captain-camel <cameron.delong@icloud.com>
2021-10-10 11:32:40 -0600

    Added `ChartBuilder` `resultBuilder` struct

---
+++ Sources/CoreCharts/Helpers/ChartBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  ChartBuilder.swift
+//  
+//
+//  Created by Cameron Delong on 10/9/21.
+//
+
+import SwiftUI
+
+@resultBuilder struct ChartBuilder {
+    @ViewBuilder static func buildBlock<C0: Chart>(_ c0: C0) -> some View {
+        
+    }
+}
