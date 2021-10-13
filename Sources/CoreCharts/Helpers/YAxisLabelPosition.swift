bc644e904a0c19b43c86ede38166a7994d025c0e
captain-camel <cameron.delong@icloud.com>
2021-10-12 18:17:46 -0600

    Added `YAxisLabelPosition` enum

---
+++ Sources/CoreCharts/Helpers/YAxisLabelPosition.swift
@@ -0,0 +1,14 @@
+//
+//  YAxisLabelPosition.swift
+//  
+//
+//  Created by Cameron Delong on 10/12/21.
+//
+
+/// Posible positions for the Y axis labels on a chart.
+public enum YAxisLabelPosition {
+    /// Left of the chart.
+    case left
+    /// Right of the chart.
+    case right
+}
