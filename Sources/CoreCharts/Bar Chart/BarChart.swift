6c13d68f59ededaabde4eec1ff76ad880b59fc6a
captain-camel <cameron.delong@icloud.com>
2021-10-22 16:21:57 -0600

    Fixed divide by zero errors in `BarChart`

--- Sources/CoreCharts/Bar Chart/BarChart.swift
+++ Sources/CoreCharts/Bar Chart/BarChart.swift
@@ -29,24 +29,35 @@ public struct BarChart: Chart {
     var yAxisLabelsPosition: YAxisLabelPosition = .left
     
     /// The color of the labels corresponding to the chart.
-    var labelColor: Color { style.startColor }
+    var yAxisLabelColor: Color { style.labelColor }
+    
+    /// Whether to display the background of the chart.
+    var showBackground = true
     
     /// The size of the view.
     @State private var size = CGSize.zero
     
     /// The space between each bar.
     private var barSpacing: CGFloat {
-        1 / CGFloat(data.count) * 90
+        if data.count == 0 {
+            return 0
+        }
+        
+        return 1 / CGFloat(data.count) * 90
     }
     
     /// The width of the bars in the chart.
     private var barWidth: CGFloat {
-        min(size.width / CGFloat(data.count) - barSpacing, 100)
+        if data.count == 0 {
+            return 0
+        }
+        
+        return min(size.width / CGFloat(data.count) - barSpacing, 100)
     }
     
     // MARK: Initializers
     /// Creates a bar chart from an array of `Double`s
-    /// 
+    ///
     /// - Parameter data: The data to display.
     public init(data: [Double]) {
         self.data = data
@@ -54,7 +65,8 @@ public struct BarChart: Chart {
     }
     
     // MARK: Body
-    public var body: some View {
+    /// The bar chart with no background.
+    var content: some View {
         ZStack(alignment: .bottom) {
             HStack(alignment: .bottom, spacing: barSpacing) {
                 ForEach(data, id: \.self) { value in
@@ -71,7 +83,6 @@ public struct BarChart: Chart {
                 path.addLine(to: CGPoint(x: size.width, y: getHeight(value: max(bounds.upperBound, 0))))
             }
             .stroke(Color(.systemGray3), style: StrokeStyle(lineWidth: 1.5, lineCap: .round))
-            .zIndex(1000)
         }
         .readSize($size)
     }
@@ -79,12 +90,18 @@ public struct BarChart: Chart {
     // MARK: Methods
     /// Returns the height of a bar based on its value.
     private func getHeight(value: Double) -> CGFloat {
-        value / (max(bounds.upperBound, 0) - min(bounds.lowerBound, 0)) * size.height
+        let range = max(bounds.upperBound, 0) - min(bounds.lowerBound, 0)
+        
+        if range == 0 {
+            return 0
+        }
+        
+        return value / range * size.height
     }
 }
 
 struct BarChart_Previews: PreviewProvider {
     static var previews: some View {
-        BarChart(data: [1,2,3,4,5,4,3,2,1])
+        BarChart(data: (0..<30).map { _ in Double.random(in: -100...100) })
     }
 }
