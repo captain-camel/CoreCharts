f72379c1dd6af175b15b694c3798d9f25bb6ccfc
captain-camel <cameron.delong@icloud.com>
2021-10-23 11:14:26 -0600

    Fixed division by zero errors in `LineChart` view

--- Sources/CoreCharts/Line Chart/LineChart.swift
+++ Sources/CoreCharts/Line Chart/LineChart.swift
@@ -35,7 +35,13 @@ public struct LineChart: Chart {
     var yAxisLabelsPosition: YAxisLabelPosition = .left
     
     /// The color of the labels corresponding to the chart.
-    var labelColor: Color { style.startColor }
+    var yAxisLabelColor: Color { style.labelColor }
+    
+    /// Whether to display the background of the chart.
+    var showBackground = true
+    
+    /// Whether to feather the gradient near the edges of the chart.
+    var featherGradient = true
     
     /// The size of the view.
     @State private var size = CGSize.zero
@@ -51,7 +57,11 @@ public struct LineChart: Chart {
     
     /// The vertical length of a different of 1 in the data.
     private var stepHeight: CGFloat {
-        size.height / (bounds.upperBound - bounds.lowerBound)
+        if bounds.upperBound - bounds.lowerBound == 0 {
+            return 0
+        }
+        
+        return size.height / (bounds.upperBound - bounds.lowerBound)
     }
     
     /// The `Path` of the line showing the data.
@@ -61,7 +71,7 @@ public struct LineChart: Chart {
     
     /// The `Path` of the space beneath the line showing the data.
     private var closedPath: Path {
-        curved ? Path.quadClosedCurvedPathWithPoints(points: data, step: CGSize(width: stepWidth, height: stepHeight)) : Path.closedLinePathWithPoints(points: data, step: CGSize(width: stepWidth, height: stepHeight))
+        curved ? Path.quadClosedCurvedPathWithPoints(points: data, step: CGSize(width: stepWidth, height: stepHeight), offset: ((data.min() ?? 0) - bounds.lowerBound) * stepHeight) : Path.closedLinePathWithPoints(points: data, step: CGSize(width: stepWidth, height: stepHeight), offset: ((data.min() ?? 0) - bounds.lowerBound) * stepHeight)
     }
     
     // MARK: Initializers
@@ -71,7 +81,8 @@ public struct LineChart: Chart {
     }
     
     // MARK: Body
-    public var body: some View {
+    /// The line chart with no background.
+    var content: some View {
         ZStack {
             Group {
                 if style.gradientColor != .clear {
@@ -84,17 +95,19 @@ public struct LineChart: Chart {
                             )
                         )
                     
-                    closedPath
-                        .fill(
-                            LinearGradient(
-                                stops: [Gradient.Stop(color: Color(.systemBackground), location: 0),
-                                        Gradient.Stop(color: Color(.systemBackground).opacity(0), location: 0.05),
-                                        Gradient.Stop(color: Color(.systemBackground).opacity(0), location: 0.95),
-                                        Gradient.Stop(color: Color(.systemBackground), location: 1)],
-                                startPoint: UnitPoint(x: 0, y: 1),
-                                endPoint: UnitPoint(x: 1, y: 1)
+                    if featherGradient {
+                        closedPath
+                            .fill(
+                                LinearGradient(
+                                    stops: [Gradient.Stop(color: Color(.systemBackground), location: 0),
+                                            Gradient.Stop(color: Color(.systemBackground).opacity(0), location: 0.05),
+                                            Gradient.Stop(color: Color(.systemBackground).opacity(0), location: 0.95),
+                                            Gradient.Stop(color: Color(.systemBackground), location: 1)],
+                                    startPoint: UnitPoint(x: 0, y: 1),
+                                    endPoint: UnitPoint(x: 1, y: 1)
+                                )
                             )
-                        )
+                    }
                 }
                 
                 linePath
@@ -110,5 +123,11 @@ public struct LineChart: Chart {
             .offset(y: (bounds.upperBound - (data.max() ?? 0)) * stepHeight)
         }
         .readSize($size)
+    }
+}
+
+struct LineChart_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChart(data: (0..<30).map { _ in Double.random(in: -100...100) })
     }
 }
