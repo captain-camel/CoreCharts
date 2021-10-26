80c71148ad09cf5b6d1d1dd7a42ce50482448367
captain-camel <cameron.delong@icloud.com>
2021-10-25 19:28:14 -0600

    Updated documentation for `YAxisLabelPosition` enum

--- Sources/CoreCharts/Helpers/YAxisLabelPosition.swift
+++ Sources/CoreCharts/Helpers/YAxisLabelPosition.swift
@@ -5,7 +5,13 @@
 //  Created by Cameron Delong on 10/12/21.
 //
 
-/// Posible positions for the Y axis labels on a chart.
+/// Possible positions for the Y axis labels on a chart.
+///
+/// To set the position of the Y axis labels for a chart, use the
+/// `labels(position:)` modifier.
+///
+///     BarChart(data: barChartData)
+///         .labels(position: .right)
 public enum YAxisLabelPosition {
     /// Left of the chart.
     case left
