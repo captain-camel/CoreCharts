f84b6c800d3dcd4686789a5fc6262395663c8126
captain-camel <cameron.delong@icloud.com>
2021-10-27 17:05:27 -0600

    Overladed `+` operator to support adding `Chart`s

---
+++ Sources/CoreCharts/Helpers/ChartAddition.swift
@@ -0,0 +1,25 @@
+//
+//  ChartAddition.swift
+//  
+//
+//  Created by Cameron Delong on 10/27/21.
+//
+
+import SwiftUI
+
+/// Combines 2 `Chart`s into one with a shared background.
+public func +<C0: Chart, C1: Chart>(lhs: C0, rhs: C1) -> some View {
+    ChartView {
+        lhs
+        
+        rhs
+    }
+}
+
+struct ChartAddition_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChart(data: [1, 2, 3])
+            .style(.orangeGlow) +
+        LineChart(data: [3, 1, 2])
+    }
+}
